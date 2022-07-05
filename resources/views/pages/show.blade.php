e6060351d5f7054de7b69b8a8f74a1ae2316b61b
jocelinkisenga <kisengajocelin7@gmail.com>
2022-07-05 14:25:04 +0200

    added show method

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,20 +1,64 @@
 <!DOCTYPE html>
- <html lang="en">
- 	<head>
- 		<!-- Required meta tags -->
- 		<meta charset="utf-8">
- 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- 		<title>Page Title</title>
- 
- 		<!-- Bootstrap CSS -->
- 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
- 	</head>
- 	<body>
- 		<h1>{{$post->title}}</h1>
- 			<p>{!! $post->body !!}</p>
- 		<!-- Optional JavaScript -->
- 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
- 		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
- 		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
- 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script></body>
- </html>
+	<html lang="en">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+			<meta name="description" content="">
+			<meta name="author" content="">
+			<link rel="icon" href="../../../../favicon.ico">
+	
+			<title>{{$post->title}}</title>
+	
+			<!-- Bootstrap core CSS -->
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+			<!-- Custom styles for this template -->
+			<link href="blog.css" rel="stylesheet">
+		</head>
+	
+		<body>
+		<div class="container">
+			 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+			   <div class="container-fluid">
+			    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			      <span class="navbar-toggler-icon"></span>
+			    </button>
+			    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			      <a class="navbar-brand" href="#">Hidden brand</a>
+			      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+			        <li class="nav-item">
+			          <a class="nav-link active" aria-current="page" href="#">Home</a>
+			        </li>
+			        <li class="nav-item">
+			          <a class="nav-link" href="#">Link</a>
+			        </li>
+			        <li class="nav-item">
+			          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+			        </li>
+			      </ul>
+			      <form class="d-flex">
+			        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+			        <button class="btn btn-outline-success" type="submit">Search</button>
+			      </form>
+			    </div>
+			  </div>
+			</nav>
+
+			<div class="card mb-3">
+		  	<img src="..." class="card-img-top" alt="...">
+		  		<div class="card-body">
+		    		<h5 class="card-title">{{$post->title}}</h5>
+		   			 <p class="card-text">{!! $post->body !!}</p>
+		    		<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+		  		</div>
+			</div>
+
+
+
+
+		</div>
+	
+	
+			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+			</body>
+	</html>
